4b8031ece1b920767f7a2af3acccd73243f68bcd
Noé <noedelamora@gmail.com>
2024-05-15 00:04:47 -0500

    Add simple search functionality to find path between a member and an expert, no search box included yet

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -42,6 +42,43 @@ class MembersController < ApplicationController
     end
   end
 
+  def search
+    # clear the flash so the correct response is displayed
+    flash[:error] = nil
+    # TODO: Add better error handling and user feedback
+    flash[:error] = 'Query not found' and (return render :search) unless params[:query].present?
+
+    @query = params[:query]
+    @member = Member.find(params[:id])
+
+    flash[:error] = 'Member not found' and return unless @member.present?
+
+    friend_ids = @member.friendships.pluck(:friend_id)
+    @expert = Member.joins(:headers).where('headers.content LIKE ?', "%#{@query}%").where.not(id: friend_ids + [@member.id]).first
+
+    flash[:error] = 'No expert found' and return unless @expert.present?
+
+    # TODO: Move this into a concern, although so far it's only used in one place, at least for the scope in a different method
+    visited = {}
+    queue = [[@member, [@member]]]
+
+    # BFS to find a connection between a member doing the search and an expert
+    while queue.present?
+      current_member, @friend_path = queue.shift
+      return @friend_path if current_member == @expert
+
+      current_member.friends.each do |friend|
+        unless visited[friend.id]
+          visited[friend.id] = true
+          queue << [friend, @friend_path + [friend]]
+        end
+      end
+    end
+
+    # Return an empty path if no connection exists between member and expert
+    @friend_path = []
+  end
+
   private
 
   def member_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :members, only: [:index, :show, :new, :create]
+  resources :members, only: [:index, :show, :new, :create] do
+    member do
+      get 'search'
+    end
+  end
+
   resources :friendships, only: [:new, :create]
 
   root 'members#index'
