b618291312717815752e170768baa714e6f5c96e
Noé <noedelamora@gmail.com>
2024-05-16 21:02:12 -0500

    updated search unit tests to provide a depth of four connections

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -5,9 +5,10 @@ describe 'Members', type: :request do
   let(:headers) { { "Accept" => "application/json", 'Content-Type' => 'application/json' } }
 
   let(:member1) { Member.create!(first_name: 'Test', last_name: 'One', url: 'https://example.com') }
-  let(:member2) { Member.create!(first_name: 'Test', last_name: 'Two', url: 'https://google.com') }
-  let(:member3) { Member.create!(first_name: 'Test', last_name: 'Three', url: 'https://everlyhealth.com') }
+  let(:member2) { Member.create!(first_name: 'Test', last_name: 'Two', url: 'https://everlyhealth.com') }
+  let(:member3) { Member.create!(first_name: 'Test', last_name: 'Three', url: 'https://google.com') }
   let(:member4) { Member.create!(first_name: 'Test', last_name: 'Four', url: 'https://some-url.com') }
+  let(:member5) { Member.create!(first_name: 'Test', last_name: 'Five', url: 'https://stackoverflow.com') }
 
   describe 'creating a member' do
     subject { post '/members', params: params.to_json, headers: headers }
@@ -74,28 +75,31 @@ describe 'Members', type: :request do
   end
 
   describe 'searching for an expert' do
-    let(:member3_content) { member3.headers.first.content.split(' | ').first }
+    let(:member2_content) { member2.headers.first.content.split(' | ').first }
     let(:member4_content) { member4.headers.first.content.split(' | ').first }
+    let(:member5_content) { member5.headers.first.content.split(' | ').first }
+    
 
     before do
       Friendship.create!(member_id: member1.id, friend_id: member2.id)
       Friendship.create!(member_id: member2.id, friend_id: member3.id)
+      Friendship.create!(member_id: member3.id, friend_id: member4.id)
     end
 
     context 'when member exists and query is present' do
-      subject { get "/members/#{member1.id}/search", params: { query: member3_content }, headers: headers }
+      subject { get "/members/#{member1.id}/search", params: { query: member4_content }, headers: headers }
 
       it 'returns the friend path to expert' do
         subject
 
         expect(response).to have_http_status(:success)
         expect(body).to be_an_instance_of(Array)
-        expect(body).to eq([member1, member2, member3].map(&:as_json))
+        expect(body).to eq([member1, member2, member3, member4].map(&:as_json))
       end
     end
 
     context 'when member is not present' do
-      subject { get "/members/0/search", params: { query: member3_content }, headers: headers }
+      subject { get "/members/0/search", params: { query: member4_content }, headers: headers }
 
       it 'returns appropriate error response' do
         subject
@@ -119,7 +123,7 @@ describe 'Members', type: :request do
     end
 
     context 'when no expert is found' do
-      subject { get "/members/#{member1.id}/search", params: { query: 'asdf' }, headers: headers }
+      subject { get "/members/#{member1.id}/search", params: { query: member2_content }, headers: headers }
 
       it 'returns appropriate error response' do
         subject
@@ -131,7 +135,7 @@ describe 'Members', type: :request do
     end
 
     context 'when no friend path is found' do
-      subject { get "/members/#{member1.id}/search", params: { query: member4_content }, headers: headers }
+      subject { get "/members/#{member1.id}/search", params: { query: member5_content }, headers: headers }
 
       it 'returns appropriate error response' do
         subject
